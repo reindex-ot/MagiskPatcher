b7e40820a3eb8ae236dc449b3392dd0a1e131db9
affggh <879632264@qq.com>
2025-06-06 19:25:45 +0800

    ui: optmize looks

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@
 import "./dark.css";
 import "./light.css";
 import "./style.css";
-import "./fonts.css";
 
 import "@material/web/all.js";
 
@@ -15,29 +14,39 @@ import axios from "axios";
 import JSZip from "jszip";
 
 // @ts-ignore
-const __version__ = "0.1.3";
+const __version__ = "0.1.4";
 // @ts-ignore
 const __author__ = "affggh";
+
+const logo = `
+<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+  <!-- 红色粗圆 -->
+  <!--circle cx="50" cy="50" r="40" stroke="red" stroke-width="8" fill="none" /-->
+  
+  <!-- 货币符号 -->
+  <text x="50" y="80" font-family="Arial" font-size="80" text-anchor="middle">⭕</text>
+  <text x="50" y="65" font-family="Arial" font-size="60" text-anchor="middle">💰</text>
+</svg>
+`
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */`
     <header class="appbar">
         <div class="row">
             <md-icon style="--md-icon-size: 36px" tabindex="-1">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                <!--svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                     fill="#e3e3e3">
                     <path
                         d="M40-240q9-107 65.5-197T256-580l-74-128q-6-9-3-19t13-15q8-5 18-2t16 12l74 128q86-36 180-36t180 36l74-128q6-9 16-12t18 2q10 5 13 15t-3 19l-74 128q94 53 150.5 143T920-240H40Zm240-110q21 0 35.5-14.5T330-400q0-21-14.5-35.5T280-450q-21 0-35.5 14.5T230-400q0 21 14.5 35.5T280-350Zm400 0q21 0 35.5-14.5T730-400q0-21-14.5-35.5T680-450q-21 0-35.5 14.5T630-400q0 21 14.5 35.5T680-350Z" />
-                </svg>
+                </svg-->
+                ${logo}
             </md-icon>
             <div class="appbar-title">MagiskPatcher</div>
             <div class="spacer"></div>
-            <md-text-button style="padding: 10px;" href="https://github.com/CircleCashTeam" trailing-icon>
-                Github
-                <svg slot="icon" viewBox="0 0 48 48">
-                    <path
-                        d="M9 42q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6h13.95v3H9v30h30V25.05h3V39q0 1.2-.9 2.1-.9.9-2.1.9Zm10.1-10.95L17 28.9 36.9 9H25.95V6H42v16.05h-3v-10.9Z" />
-                </svg>
-            </md-text-button>
+            <md-icon-button style="--md-icon-button-icon-size: 36px" href="https://github.com/CircleCashTeam">
+                <md-icon><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 64 64">
+                  <path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z"></path>
+                </svg></md-icon>
+            </md-icon-button>
         </div>
     </header>
 
@@ -121,7 +130,7 @@ if (darkModeMediaQuery.matches) {
 }
 
 const terminal = new Terminal({
-  fontFamily: "'Iosevka Web', 'Cascadia Code', 'Consolas', 'Courier New', monospace",
+  fontFamily: "'Cascadia Code', 'Consolas', 'Courier New', monospace",
   fontSize: 14,
   letterSpacing: 0,
   theme: {
@@ -640,4 +649,5 @@ async function loadApkNeeded(buffer: ArrayBuffer, arch: string): Promise<Needed 
 terminal.writeln("\x1b[96mMagisk Patcher on WEB");
 terminal.writeln("\x1b[93mAuthor: \x1b[94m" + __author__);
 terminal.writeln("\x1b[93mVersion:\x1b[94m " + __version__);
+terminal.writeln("\x1b[92mSupport:\x1b[94m Magisk v22.0+");
 terminal.writeln("\x1b[0m");
